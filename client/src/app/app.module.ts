7c3506025efbd6276427d10d9997950afe180ff9
AdamMomen <adammuman81@gmail.com>
2020-01-15 19:59:23 +0100

    added search to sideBar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './components/user/user.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CategroyComponent } from './components/categroy/categroy.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { ProductComponent } from './components/product/product.component';
 
 import { AppComponent } from './app.component';
 
@@ -10,13 +15,19 @@ import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ProductsComponent,
+    CategroyComponent,
+    SideBarComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
-    FormsModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/side-bar/side-bar.component.ts
+++ client/src/app/components/side-bar/side-bar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import $ from 'jquery'
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-
-
-  constructor() { }
+  constructor() {
+  }
+  @Input() user: String
+  clicker() {
+    $("#sidebar").toggleClass("visible");
+  }
   ngOnInit() {
   }
-
 }
