0273bab9f3510f97dae48ae71ece681b03aae39f
Mohamed-Dhia <dhiaweslati16@gmail.com>
2020-01-16 17:04:32 +0100

    prepared some back end functions

---
+++ back end/routes/config/passport.js
@@ -0,0 +1,26 @@
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJWT = require("passport-jwt").ExtractJwt;
+
+const User = require("../../database/userDB");
+const config = require("./config");
+
+module.exports = function(passport) {
+  let opts = {};
+  opts.jwtFromRequest = ExtractJWT.fromAuthHeaderWithScheme("jwt");
+  opts.secretOrKey = config.secret;
+
+  passport.use(
+    new JwtStrategy(opts, function(jwt_payload, done) {
+      User.findById(jwt_payload._id, function(err, user) {
+        if (err) {
+          return done(err, false);
+        }
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+    })
+  );
+};
