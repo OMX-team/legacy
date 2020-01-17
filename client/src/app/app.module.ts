2b10e36f4630244daff18ff0ed532ea165f059db
guizenifarouk <guizeni.farouk95@gmail.com>
2020-01-17 10:29:51 +0100

    finished rating back and from , still star styling | working on products per user

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,24 @@
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { Routes, RouterModule } from "@angular/router";
+import { UserComponent } from "./components/user/user.component";
+import { ProductsComponent } from "./components/products/products.component";
+import { CategroyComponent } from "./components/categroy/categroy.component";
+import { SideBarComponent } from "./components/side-bar/side-bar.component";
+import { ProductComponent } from "./components/product/product.component";
+import { AppComponent } from "./app.component";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { FormsModule } from "@angular/forms";
+import { AngularMaterialModule } from "./modules/angular-material/angular-material.module";
+import { AddProductComponent } from "./components/add-product/add-product.component";
+import { ProductCardComponent } from "./components/product-card/product-card.component";
+import { ProfilComponent } from "./components/profil/profil.component";
+import { StartRatingComponent } from "./components/start-rating/start-rating.component";
+import { ServicesService } from "./components/services/services.service";
 
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UserComponent } from './components/user/user.component';
-import { ProductsComponent } from './components/products/products.component';
-import { CategroyComponent } from './components/categroy/categroy.component';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { ProductComponent } from './components/product/product.component';
-import { AppComponent } from './app.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FormsModule } from '@angular/forms';
-import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
-import { AddProductComponent } from './components/add-product/add-product.component';
-
+const appRoutes: Routes = [{ path: "profile/:id", component: ProfilComponent }];
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,18 +28,24 @@ import { AddProductComponent } from './components/add-product/add-product.compon
     SideBarComponent,
     ProductComponent,
     AddProductComponent,
-
+    ProductCardComponent,
+    ProfilComponent,
+    StartRatingComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
     AngularMaterialModule,
-    HttpClientModule,
+    HttpClientModule
   ],
 
-  providers: [],
+  providers: [ServicesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
