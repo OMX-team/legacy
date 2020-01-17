0035acda91c7874d4c833e37cd03445f1f66c9c7
Mohamed-Dhia <dhiaweslati16@gmail.com>
2020-01-17 11:21:12 +0100

    merged everyone's work

--- client/src/app/components/user/user.component.ts
+++ client/src/app/components/user/user.component.ts
@@ -1,25 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { RequestHandlerService } from '../../services/request-handler.service'
+import { Component, OnInit } from "@angular/core";
 
-import $ from 'jquery'
+import $ from "jquery";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
 export class UserComponent implements OnInit {
   username: String = "Adam";
 
-  constructor(
-  ) { }
+  constructor() {}
   ngOnInit() {
-    this.hideSideNav()
-
+    this.hideSideNav();
   }
   hideSideNav() {
-    $('#sidebar').hasClass('visible') || !$('#sidebar').hasClass() ?
-      $("#sidebar").attr("class", "") : ''
+    $("#sidebar").hasClass("visible") || !$("#sidebar").hasClass()
+      ? $("#sidebar").attr("class", "")
+      : "";
   }
-
 }
