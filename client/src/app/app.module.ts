d5bf2718a6e67c76ea60a9ce478bfe2b7dff33ca
AdamMomen <adammuman81@gmail.com>
2020-01-18 10:46:05 +0100

    merged

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,31 +22,30 @@ import { FirstPageComponent } from "./components/first-page/first-page.component
 import { SecondPageComponent } from "./components/second-page/second-page.component";
 
 import { AuthService } from "./components/auth-service/auth.service";
-const appRoutes: Routes = [{ path: "profile/:id", component: ProfilComponent }];
 import { HomeComponent } from "./components/home/home.component";
 
 
-// const appRoutes: Routes = [
+const appRoutes: Routes = [
 
 
-//   {
-//     path: "home", component: HomeComponent, children:
-//       [
-//         { path: "quick", component: SecondPageComponent },
-//       ]
-//   },
-//   { path: '', redirectTo: 'home', pathMatch: 'full' },
-//   {
-//     path: "dashboard", component: UserComponent,
-//     children: [
-//       { path: "add-product", component: AddProductComponent },
-//       { path: "category", component: CategroyComponent },
-//       {
-//         path: "products", component: ProductsComponent
-//       },
-//     ]
-//   },
-// ];
+  {
+    path: "home", component: HomeComponent, children:
+      [
+        { path: "quick", component: SecondPageComponent },
+      ]
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: "dashboard", component: UserComponent,
+    children: [
+      { path: "add-product", component: AddProductComponent },
+      { path: "category", component: CategroyComponent },
+      {
+        path: "products", component: ProductsComponent
+      },
+    ]
+  },
+];
 
 
 @NgModule({
@@ -80,4 +79,4 @@ import { HomeComponent } from "./components/home/home.component";
   providers: [ServicesService, AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
