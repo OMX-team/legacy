f3b840751d73bebea68d4789027fa13bb21012e7
AdamMomen <adammuman81@gmail.com>
2020-01-19 19:29:45 +0100

    finished email verf

--- back end/emailValidation/emailSender.js
+++ back end/emailValidation/emailSender.js
@@ -1,26 +1,29 @@
 require('dotenv').config()
-var sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('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');
-// console.log(process.env.SENDGRID_API_KEY)
-/**
- username @params 
- URL @params 
- */
-function sendEmail(
-    // to = "adammuman81@gmail.com", username = "adam", code = "35sdfsd"
-) {
+const send = require('gmail-send')({
+    user: 'omx.rbk@gmail.com',
+    pass: 'azruraakmmpjorft',
+});
+
+function sendEmail(to, username, code) {
     // check the email before before sending
     var msg = {
-        to: 'adammuman81@gmail.com',
-        from: 'OMX_DO_NOT_REPLY@OMX.com',
-        subject: 'Verify_Your_Email',
-        text: 'Email Veritfication',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    return sgMail.send(msg)
+
+        to: `${to}`,
+        subject: 'Account Vertification',
+        text: 'Verify Your Account',
+        html: `
+       <div><h1>Hello ${username}</h1><br/>
+       <strong><h2>Thanks for registering to our website</h2></strong><br/>
+       <h5>This is your vertification code :</h5><br/>
+       <strong><h1>${code}</h1></strong>
+       </div>`,
+    }
+    return send(msg)
 }
-sendEmail().then(() => {
-    console.log('message sent')
+sendEmail().then(({
+    result
+}) => {
+    console.log('message sent', result)
 })
 module.exports = {
     sendEmail
