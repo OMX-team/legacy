380e38c83cf46dd517b120935ef46223bfbbe62c
AdamMomen <adammuman81@gmail.com>
2020-03-13 22:56:49 +0100

    (clean) files

--- back end/database/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  db: "mongodb+srv://OMX:the6glcmab@cluster0-mq83j.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true"
-};
